de7b8f40c47f880661b8284d9b73afb841908bdf
Erin Carter <me@erincarter.com>
2012-02-13 15:37:34 -0600

    #build() update
    added mkdir -p to create directory path. moved build execution to
    build() function. updated test.

--- test/build.test.js
+++ test/build.test.js
@@ -6,14 +6,18 @@ describe('#build()', function(){
   var buildTest;
   
   before(function(){
-    buildTest = { parsed: 
-       { body: '\n## Welcome to the Site\n\nHave a look around and let us know what you think!\n',
-         title: 'Welcome',
-         author: 'E. Carter',
-         date: '2012-02-09',
-         view: 'home',
-         content: '<h2>Welcome to the Site</h2>\n\n<p>Have a look around and let us know what you think!</p>' },
-      compiled: '<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="utf-8">\n  <title>{{ title }}</title>\n</head>\n<body>\n  <div id="home-content">\n  <h2>Welcome to the Site</h2>\n\n<p>Have a look around and let us know what you think!</p>\n</div>\n</body>\n</html>\n' };
+    buildTest = {
+      parsed: {
+          body: '\n## Welcome to the Site\n\nHave a look around and let us know what you think!\n',
+          title: 'Welcome',
+          author: 'E. Carter',
+          date: '2012-02-09',
+          view: 'home',
+          content: '<h2>Welcome to the Site</h2>\n\n<p>Have a look around and let us know what you think!</p>'
+      },
+      compiled: '<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="utf-8">\n  <title>{{ title }}</title>\n</head>\n<body>\n  <div id="home-content">\n  <h2>Welcome to the Site</h2>\n\n<p>Have a look around and let us know what you think!</p>\n</div>\n</body>\n</html>\n',
+      saved: 'examples/site/public/index.html'
+    };
   })
   
   describe('build file using `setup` properties', function(){
@@ -30,6 +34,7 @@ describe('#build()', function(){
           'examples/site/src/index.md'
         , {}
         , function (generated) {
+            buildTest.saved = 'examples/site/src/index.html';
             generated.should.eql( buildTest );
             fs.readFile('examples/site/src/index.html', function (err, data) {
               if (err) done(err);
@@ -55,6 +60,7 @@ describe('#build()', function(){
           'examples/site/src/index.md'
         , { dest: 'examples/site/public' }
         , function (generated) {
+            buildTest.saved = 'examples/site/public/index.html';
             generated.should.eql( buildTest );
             fs.readFile('examples/site/public/index.html', function (err, data) {
               if (err) done(err);
